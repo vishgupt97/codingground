7ea11675e86d5be70c98bb64f615911329322bf6
vishgupt97 <jhaampu@gmail.com>
2016-06-02 02:58:10 +0530

    Created by Coding Ground Client

---
+++ New Project-20160601/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class HelloWorld
+
+{
+
+public static void main(String args[])
+
+{
+
+employee vishwas = new employee("vishwas",22,"TL");
+employee dev = new employee("dev",21,"TL");
+vishwas.printempoyee();
+}
+
+}
